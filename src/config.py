cb3d0141a73fe53bb233a9500f04b627c61a4da3
Abdelrahman Nashat <47497135+abdelrahmanashat@users.noreply.github.com>
2026-09-06 16:13:12 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -7,17 +7,19 @@ class Settings(BaseSettings):
     JWT_SECRET : str
     JWT_ALGORITHM : str
     REDIS_URL : str = "redis://localhost:6379/0"
-    MAIL_USERNAME: str
-    MAIL_PASSWORD: str
-    MAIL_FROM: str
-    MAIL_PORT: int
-    MAIL_SERVER: str
-    MAIL_FROM_NAME: str
-    MAIL_STARTTLS: bool = True
-    MAIL_SSL_TLS: bool = False
-    USE_CREDENTIALS: bool = True
-    VALIDATE_CERTS: bool = True
     DOMAIN: str
+    MAIL_API_KEY : str
+    MAIL_FROM: str
+    MAIL_FROM_NAME: str
+    
+    # MAIL_USERNAME: str
+    # MAIL_PASSWORD: str
+    # MAIL_PORT: int
+    # MAIL_SERVER: str
+    # MAIL_STARTTLS: bool = True
+    # MAIL_SSL_TLS: bool = False
+    # USE_CREDENTIALS: bool = True
+    # VALIDATE_CERTS: bool = True
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
     
 Config = Settings()
